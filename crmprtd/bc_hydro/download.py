fc8615795b9e97b0d871e0de2064a54933ca1b20
cairosanders <cairosanders@hotmail.com>
2020-09-16 16:15:47 -0700

    remove temp file and print read from txt file to stdout

--- crmprtd/bc_hydro/download.py
+++ crmprtd/bc_hydro/download.py
@@ -1,7 +1,7 @@
 # Downloads data from BC hyrdo
 
 import pysftp
-from tempfile import TemporaryDirectory, SpooledTemporaryFile
+from tempfile import TemporaryDirectory
 import logging
 import os
 import sys
@@ -16,34 +16,22 @@ log = logging.getLogger(__name__)
 def download(username, gpg_private_key, ftp_server, ftp_dir):
     # Connect FTP server and retrieve file
     try:
-        sftp = pysftp.Connection(ftp_server, username=username, 
-                                 private_key = gpg_private_key)
+        sftp = pysftp.Connection(ftp_server, username=username,
+                                 private_key=gpg_private_key)
         with TemporaryDirectory() as tmp_dir:
             sftp.get_r(ftp_dir, tmp_dir)
             os.chdir(os.path.expanduser(tmp_dir + '/' + ftp_dir))
 
-            with SpooledTemporaryFile(
-                    max_size=int(os.environ.get('CRMPRTD_MAX_CACHE', 2**20)),
-                    mode='r+') as tempfile:
+            for filename in os.listdir(os.getcwd()):
+                name, extension = os.path.splitext(filename)
+                if extension == '.zip':
+                    zip_file = ZipFile(filename, 'r')
+                    for name in zip_file.namelist():
+                        txt_file = zip_file.open(name)
+                        sys.stdout.buffer.write(txt_file.read())
 
-                for filename in os.listdir(os.getcwd()):
-                    name, extension = os.path.splitext(filename)
-                    if extension == '.zip':
-                        zip_file = ZipFile(filename, 'r')
-                        zip_file.extractall()
-                        for txt_file in os.listdir(os.getcwd()):
-                            name, extension = os.path.splitext(txt_file)
-                            if extension == '.txt':
-                                file = open(txt_file, mode='r')
-                                tempfile.write(file.read())
-                                os.remove(os.getcwd() + '/' + txt_file)                
-                tempfile.seek(0)
-                for line in tempfile.readlines():
-                    sys.stdout.buffer.write(line.encode('utf-8'))
-
-    except Exception as e:
+    except Exception:
         log.exception("Unable to process ftp")
-        print(e)
 
 
 def main():
@@ -54,19 +42,19 @@ def main():
     parser.add_argument('-f', '--ftp_server',
                         default='sftp2.bchydro.com',
                         help=('Full uri to BC Hydro\'s ftp server'))
-
     parser.add_argument('-F', '--ftp_dir',
                         default=('pcic'),
                         help=('FTP Directory containing BC hydro\'s '
                               'data files'))
     parser.add_argument('-g', '--gpg_private_key',
-                        help=('Path to file with GPG private key'))                         
+                        help=('Path to file with GPG private key'))
     args = parser.parse_args()
 
     setup_logging(args.log_conf, args.log_filename, args.error_email,
                   args.log_level, 'crmprtd.bc_hydro')
 
-    download(args.username, args.gpg_private_key, args.ftp_server, args.ftp_dir)
+    download(args.username, args.gpg_private_key, args.ftp_server,
+             args.ftp_dir)
 
 
 if __name__ == "__main__":
